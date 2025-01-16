56d12738403f78cc2a27a82278870a539dd57536
FrancescoEsk <francifrison.ff@gmail.com>
2025-01-16 18:30:16 +0100

    Aggiungi file utils.js con funzione di moltiplicazione e utilizzo in main.js

--- TEST-PROJECT/main.js
+++ TEST-PROJECT/main.js
@@ -1,1 +1,20 @@
 console.log('Hello world');
+
+const utils = require("./utils");
+
+console.log(utils);
+
+const res = utils.multiply(1, 2);
+
+console.log('Res', res, utils.age);
+
+const user = {
+    id: 0,
+    name: 'Marco',
+    age: 26,
+    sayHi : () => {
+        console.log('hi');
+    }
+}
+
+user.sayHi();
